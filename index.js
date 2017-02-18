5a44457fe01f5148c122d296549b41eadbd371c6
Dominic Tassio <dominic.tassio@gmail.com>
2017-02-18 13:42:38 -0600

    update css classes

--- index.js
+++ index.js
@@ -2,29 +2,29 @@ const purple = "#ff00bf";
 const orange = "#ffbf00";
 
 exports.decorateConfig = (config) => {
-    return Object.assign({}, config, {
-        borderColor: 'black',
-        css: `
-            ${config.css || ''}
-            .hyper_main {
-                border-top-color: ${purple} !important;
-                border-bottom-color: ${orange} !important;
-            }
-            .hyper_main:before,
-            .hyper_main:after {
-                content: "";
-                position: absolute;
-                background-image: linear-gradient(${purple}, ${orage});
-                top: -1px;
-                bottom: -1px;
-                width: 1px;
-            }
-            .hyper_main:before {
-                left: -1px;
-            }
-            .hyper_main:after {
-                right: -1px;
-            }
-        `,
-	});
-};
+  return Object.assign({}, config, {
+    borderColor: 'black',
+    css: `
+      ${config.css || ''}
+      .hyper_main.main_mmu {
+        border-top-color: ${purple} !important;
+        border-bottom-color: ${orange} !important;
+      }
+      .hyper_main.main_mmu:before,
+      .hyper_main.main_mmu:after {
+        content: "";
+        position: absolute;
+        background-image: linear-gradient(${purple}, ${orage});
+        top: -1px;
+        bottom: -1px;
+        width: 1px;
+      }
+      .hyper_main.main_mmu:before {
+        left: -1px;
+      }
+      .hyper_main.main_mmu:after {
+        right: -1px;
+      }
+    `
+  });
+}
